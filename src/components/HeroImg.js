a90b0850db7d584f820a68ec8ee3c7a171fcc78e
asalshamim <shamimasal8@gmail.com>
2023-09-18 14:31:27 +0200

    Style

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -5,25 +5,24 @@ import IntroImg from "../assets/heroImg.png";
 import { Link } from "react-router-dom";
 
 const HeroImg = () => {
-  return <div className="hero">
-
-    <div className="mask">
-      <img className="into-img" src={IntroImg} alt="hero"/>
-
-    </div>
-    <div className="content">
-      <p> HI, I'M A FRONTEND DEVELOPER </p>
-      <div>
-        <Link to="/project" className="btn">
-          Projects
-        </Link>
-
-        <Link to="/contact" className="btn btn-light">
-          Contact
-        </Link>
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="into-img" src={IntroImg} alt="hero" />
+      </div>
+      <div className="content">
+        <p>HI, I'M A FRONTEND DEVELOPER</p>
+        <div>
+          <Link to="/project" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default HeroImg;
