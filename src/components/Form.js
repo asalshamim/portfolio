cb90d5df9a43eb8ee04be4f2f84ff79d14805770
asalshamim <shamimasal8@gmail.com>
2023-10-15 20:06:58 +0200

    form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,22 @@ import "./FormStyles.css";
 import React from "react";
 
 const Form = () => {
-  return <div className="form"></div>;
+  return <div className="form">
+    <form>
+      <label>Your Name</label>
+      <input type="text"></input>
+
+      <label>Email</label>
+      <input type="email"></input>
+
+      <label>Subject</label>
+      <input type="text"></input>
+
+      <label>Message</label>
+      <textarea rows="5" placeholder="Type your message here" />
+      <button className="btn">Submit</button>
+    </form>
+  </div>;
 };
 
 export default Form;
