385a9099937c4005bcc016eaa08099c3b11c8fee
asalshamim <shamimasal8@gmail.com>
2023-10-16 20:12:29 +0200

    card

--- src/components/WorkCradData.js
+++ src/components/WorkCradData.js
@@ -10,14 +10,14 @@ const ProjectCardData = [
     },
     {
         imgsrc: pro2,
-        title: "Movie releases",
-        text: "The Movie Release App is a dynamic web application that I designed and developed using React.js, showcasing my skills in front-end web development and API integration.",
-        view: "https://movie-app-psi-mauve.vercel.app/",
+        title: "Clothing Webshop with Dark and Light Mode",
+        text: "The Clothing Webshop with Dark and Light Mode project showcases my skills in frontend and backend web development. The project is built using React.js, Sass and  Firebase.",
+        view: "https://examensarbete1.vercel.app/",
     },
     {
         imgsrc: pro3,
-        title: "Movie releases",
-        text: "Another schoolassignment made in React. A groupproject where we made an online store with both products and courses.",
+        title: "WordGame-React",
+        text: "WordGame-React is an entertaining and educational web-based word game that challenges players to test their vocabulary and spelling skills. The game is built using React.js.",
         view: "https://wordgame-react.vercel.app/",
     },
 ];
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import HeroImg from "../components/HeroImg";
 import Home2 from "../components/Home2";
+
 import Footer from "../components/Footer";
 
 const Home = () => {
@@ -9,7 +10,8 @@ const Home = () => {
     <div>
       <Navbar />
       <HeroImg />
-      <Home2/>
+      <Home2 />
+    
       <Footer/>
   </div>
   );
